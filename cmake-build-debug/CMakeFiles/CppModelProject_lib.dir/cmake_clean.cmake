520afc2de37fc1064fe96c6c533e9f8d995af934
fish <105226542+rjzhb@users.noreply.github.com>
2023-07-04 14:02:49 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/CppModelProject_lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppModelProject_lib.dir/src/MyClass/MyClass.cpp.o"
+  "libCppModelProject_lib.pdb"
+  "libCppModelProject_lib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppModelProject_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
