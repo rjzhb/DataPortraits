955619f86e92764f5290b1508f7e3444c18ce9cb
rjzhb <rjzhb222@163.com>
2023-07-10 00:41:36 +0800

    optimize

--- test/string_util_test.cpp
+++ test/string_util_test.cpp
@@ -6,11 +6,11 @@
 
 // Test calculate index distance
 TEST(StringUtilTest, TestIndexDistance) {
-    std::string s1 = "abcd";
-    std::string s2 = "cdef";
-    std::string s3 = "efgh";
-    std::string s4 = "chjk";
-    std::string s5 = "c";
+    const std::string s1 = "abcd";
+    const std::string s2 = "cdef";
+    const std::string s3 = "efgh";
+    const std::string s4 = "chjk";
+    const std::string s5 = "c";
 
     EXPECT_EQ(calculateIndexDistance(s1, s2), 2);
     EXPECT_EQ(calculateIndexDistance(s1, s3), 4);
@@ -18,28 +18,28 @@ TEST(StringUtilTest, TestIndexDistance) {
 }
 
 TEST(StringUtilTest, TestIndexDistance2) {
-    std::string k1 = "";
-    std::string k2 = "ab";
+    const std::string k1 = "";
+    const std::string k2 = "ab";
 
-    std::string a1 = "abc";
-    std::string a2 = "";
+    const std::string a1 = "abc";
+    const std::string a2 = "";
     EXPECT_EQ(calculateIndexDistance(k1, k2), 0);
     EXPECT_EQ(calculateIndexDistance(a1, a2), 3);
 }
 
 TEST(StringUtilTest, TestIndexDistance3) {
-    std::string s1 = "hel";
-    std::string s2 = "ell";
-    std::string s3 = "llo";
+    const std::string s1 = "hel";
+    const std::string s2 = "ell";
+    const std::string s3 = "llo";
 
     EXPECT_EQ(calculateIndexDistance(s1, s2), 1);
     EXPECT_EQ(calculateIndexDistance(s1, s3), 2);
 }
 
 TEST(StringUtilTest, TestSimpleChar2String) {
-    std::vector<char> block{'a', 'b', '\0', ' ', '4', '5'};
-    std::vector<char> value = std::vector<char>(block.begin(), block.end());
-    std::string str(value.begin(), value.begin() + value.size());
+    const std::vector<char> block{'a', 'b', '\0', ' ', '4', '5'};
+    const std::vector<char> value = std::vector<char>(block.begin(), block.end());
+    const std::string str(value.begin(), value.begin() + value.size());
 
     std::cout << block.data() << std::endl;
     std::cout << str << std::endl;
